1cf72b6852bac0e382cc79c8fc9a0159e8c36014
nimbus <christophercamorrison@gmail.com>
2024-02-22 15:20:12 -0500

    Added animations for the home part of the website

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 //Imports Page Components 
-import HomePage from './pages/HomePage'
+import HomePage from './pages/HomePage/HomePage.js'
 
 function App() {
   return (
---
+++ src/pages/HomePage/HomePage.js
@@ -0,0 +1,76 @@
+import './HomePage.css' 
+import React, {useState} from 'react';
+import styled from 'styled-components'
+import {motion} from 'framer-motion'
+import GithubLogo from '../../assets/github-logo.png'
+import LinkedinLogo from '../../assets/linkedin-logo.png'
+import EmailLogo from '../../assets/email-logo.png'
+
+function HomePage() {
+
+  const yoyoVariants = {
+    hover: {
+      scale: 1.2, // Scale up by 20%
+      transition: {
+        duration: 0.5, // Duration of one way of the animation
+        repeat: Infinity, // Repeat the animation infinitely
+        repeatType: "reverse", // Reverse the animation on each iteration
+        ease: "easeInOut" // Use an ease-in-out timing function for a smooth effect
+      }
+    }
+  }  
+
+  const hoverAnimation = {
+    rotateY: 360, // Complete one full rotation around the Y axis
+  };
+
+  // Define transition properties for the hover animation
+  const hoverTransition = {
+    duration: 2, // Duration for one complete spin
+    ease: "linear",
+    repeat: Infinity, // Repeat the animation infinitely
+    repeatType: "reverse", // Loop the animation without reversing
+  };
+
+  return (
+    <div id='main-div'>
+        <h1>Welcome to the Home Page!</h1>
+        <div class='icon-div'>
+          <motion.img 
+            src={GithubLogo} 
+            whileHover={{ scale: 1.1, rotate: 10 }} 
+            transition={{ 
+              type: "spring", 
+              stiffness: 100,
+              damping: 20, 
+              velocity: 3,
+            }}
+            className="icon-img"
+          />
+          <motion.img 
+            src={LinkedinLogo}
+            variants={yoyoVariants}
+            whileHover='hover'
+            class='icon-img' 
+          />
+          <motion.img 
+            src={EmailLogo}
+            whileHover={{
+              ...hoverAnimation,
+              transition: hoverTransition
+            }}
+            style={{
+              perspective: 500, // Adjust for more or less depth
+              originX: 0.5,
+              originY: 0.5, // Ensures the spin is around the center
+              // display: 'block', // Ensures the img element is treated correctly for transforms
+            }}
+            class='icon-img'  
+          />
+        </div>
+    </div>  
+    
+  );
+}
+
+export default HomePage;
